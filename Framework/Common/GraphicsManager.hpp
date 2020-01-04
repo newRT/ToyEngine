832d5112b58de50d0ad22089ad99b9a0234509a9
newbitGraphics <cgn00b2020@gmail.com>
2020-01-04 23:41:51 +0800

    Add windows platform graphics

---
+++ Framework/Common/GraphicsManager.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "IRuntimeModule.hpp"
+
+namespace TEngine
+{
+    class GraphicsManager : implements IRuntimeModule
+    {
+    public:
+        virtual ~GraphicsManager() {}
+    };
+} // namespace TEngine
